5e23895227a024a04a72456284470b77d1c7cb04
dongmei <zoe.94heather@gmail.com>
2016-05-02 13:03:11 +0800

    publish to github pages

---
+++ _site/deploy.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+git checkout -b gh-pages
+jekyll build
+git add _site -f
+git ci -am "publish to github pages"
+git push origin gh-pages:gh-pages -f
+git checkout master
+git br -D gh-pages
